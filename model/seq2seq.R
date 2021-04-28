da3bfd5b0e159f9c2ca69b53b10dbf98372a2cb2
ann081993 <ann081993@snu.ac.kr>
2021-04-28 16:12:01 +0900

    Create seq2seq.R

---
+++ model/seq2seq.R
@@ -0,0 +1,41 @@
+library(keras)
+input_shape = as.integer(c(embed - 1, length(ref_char)))
+output_dim = as.integer(length(ref_char))
+latent_dim = 64
+lstm_dim = 64
+cat("input_shape, output_dim, latent_dim, lstm_dim:", 
+    input_shape, output_dim, latent_dim, lstm_dim, "\n")
+
+# encoder
+encoder_inputs <- layer_input(shape = input_shape)
+encoder <- layer_lstm(units = lstm_dim,
+                      return_state = TRUE,
+                      unroll = unroll)
+encoder_outputs <- encoder(encoder_inputs)
+state_h <- encoder(encoder_inputs)
+state_c <- encoder(encoder_inputs)
+states <- layer_concatenate(inputs = c(state_h, state_c), axis = -1)
+neck <- layer_dense(units = latent_dim, activation = 'relu')
+neck_outputs <- neck(states)        
+
+# decoder
+decode_h <- layer_dense(units = lstm_dim, activation = 'relu')
+decode_c <- layer_dense(units = lstm_dim, activation = 'relu')
+state_h_decoded <- decode_h(neck_outputs)
+state_c_decoded <- decode_c(neck_outputs)
+encoder_states <- c(state_h_decoded, state_c_decoded)
+decoder_inputs <- layer_input(shape = input_shape)
+decoder_lstm <- layer_lstm(units = lstm_dim,
+                           return_sequences = TRUE,
+                           unroll = unroll)
+decoder_outputs <- decoder_lstm(decoder_inputs, initial_state = encoder_states)
+decoder_dense <- layer_dense(units = output_dim, activation = 'softmax')
+decoder_outputs <- decoder_dense(decoder_outputs)
+
+# define model
+model <- keras_model(c(encoder_inputs, decoder_inputs), decoder_outputs)
+model
+
+model %>% compile(loss = 'categorical_crossentropy',
+                  optimizer = optimizer_adam(lr=0.005),
+                  metrics = c('accuracy'))
